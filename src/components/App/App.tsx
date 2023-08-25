4ef80c86d73c4815d8f7d7653afce0c228e611f7
M1ster Rob0t <baks750@mail.ru>
2023-08-25 16:37:52 +0300

    Сверстал компонент SignUp

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,14 @@
-import Posts from '../Posts';
+// import Posts from '../Posts';
 import Header from '../Header';
+import SignUp from '../SignUp';
+
 import './App.css';
 
 function App() {
   return (
     <div className="app">
       <Header className="app__header" />
-      <Posts />
+      <SignUp className="app__sign-up" />
     </div>
   );
 }
